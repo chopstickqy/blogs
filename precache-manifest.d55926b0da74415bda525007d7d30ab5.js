018aaa2076cb18c774aec9fc4bf9d25a7b7e6621
chen436 <chen436@avaya.com>
2020-03-30 18:07:10 +0800

    build init

---
+++ precache-manifest.d55926b0da74415bda525007d7d30ab5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba88eab8e7caa5380112d7bce3b235e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46a5264a8b6e7d50f8cb",
+    "url": "/static/css/main.08a14ea4.chunk.css"
+  },
+  {
+    "revision": "b3afe96a4a833604d3c0",
+    "url": "/static/js/2.d8ddee98.chunk.js"
+  },
+  {
+    "revision": "839d857083738a7315e7e75e0a4b5ae9",
+    "url": "/static/js/2.d8ddee98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46a5264a8b6e7d50f8cb",
+    "url": "/static/js/main.ac970fa1.chunk.js"
+  },
+  {
+    "revision": "6025f5c06ffdfbc62149",
+    "url": "/static/js/runtime-main.b6baebed.js"
+  }
+]);
